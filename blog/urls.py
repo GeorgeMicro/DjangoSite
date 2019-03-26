7b87ec4d58dcda59607e5d3f66499f9632de5c3c
Yuxuan Zhao <GeorgeMicro@users.noreply.github.com>
2019-03-26 17:06:51 +0800

    base html added

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
+	path('', views.IndexView.as_view(), name='index'),
 	path('<int:pk>', views.PostView.as_view(), name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from django.views import generic
 from .models import Post
 # Create your views here.
-def index(request):
-    return HttpResponse("Welcome to George's Wonderland!")
-
 class PostView(generic.DetailView):
 	model = Post;
 	template_name = 'blog/post.html'
+
+class IndexView(generic.ListView):
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
+	
+	def get_queryset(self):
+		return Post.objects.all()
